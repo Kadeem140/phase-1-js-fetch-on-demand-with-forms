31e18fb29d2bf227cfe9c2d0d34363082904a418
Kadeem140 <Beckford.kadeem@yahoo.com>
2021-11-26 13:30:26 -0500

    Completed Asssignment MVp

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const init = () => {
+    const inputform = document.querySelector('form');
+
+    inputform.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        console.log(event.target.children[1].value)
+
+        fetch(`http://localhost:3000/movies/${event.target.children[1].value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+            // LOG: (3) [{…}, {…}, {…}]
+        });
+    })
   
 }
 
